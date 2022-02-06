42bdf161db56abc87d5ece22a5bf550656536558
Connor Tyrrell <connor@brewgene.com>
2022-02-06 14:45:05 -0500

    Moved the GameBoard from HomePage to a Container

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,7 +1,9 @@
 import UnassignedContainer from './UnassignedContainer/UnassignedContainer';
 import PageHeader from './PageHeader/PageHeader';
+import GameBoard from './GameBoard/GameBoard';
 
 export {
     UnassignedContainer,
     PageHeader,
+    GameBoard,
 };
